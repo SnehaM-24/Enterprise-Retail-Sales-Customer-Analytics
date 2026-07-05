0cdf9d6690d463f7fa193ca602a0d160c8ca127c
Sneha Mohan Madyapgol <snehamadyapgol@gmail.com>
2026-07-05 18:53:02 +0530

    Add files via upload

---
+++ SQL/01_Create_Database.sql
@@ -0,0 +1,9 @@
+-- 01_Database_Setup.sql
+
+DROP DATABASE IF EXISTS enterprise_retail_analytics;
+CREATE DATABASE enterprise_retail_analytics;
+USE enterprise_retail_analytics;
+
+SHOW DATABASES;
+
+SELECT DATABASE();
